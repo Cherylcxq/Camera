34e03869c52795dc81b737ae87ccedaff3c966f2
Cherylcxq <44560066+Cherylcxq@users.noreply.github.com>
2018-11-19 09:12:49 +0800

    Initial commit

---
+++ Camera/Camera.Android/MainActivity.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Plugin.CurrentActivity;
+using Android.Util;
+using System.Threading.Tasks;
+
+namespace Camera.Droid
+{
+
+    [Activity(Label = "Camera", Icon = "@mipmap/icon", Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory =true
+        ,ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        static readonly string TAG = "X:" + typeof(MainActivity).Name;
+
+      
+
+        // Launches the startup task
+       
+        // Simulates background work that happens behind the splash screen
+        
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            CrossCurrentActivity.Current.Init(this, savedInstanceState);
+            
+
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+        public override void OnBackPressed() { }
+    }
+}
